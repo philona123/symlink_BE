ca2e437da2113821422cf18d27778b7785b2c9a3
philona123 <philonasebastian44@gmail.com>
2024-09-25 23:17:52 +0530

    added text extraction from pdf

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from PyPDF2 import PdfReader
+
+def extract_text_from_pdf(file_path):
+    try:
+        reader = PdfReader(file_path)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text()
+        return text
+    except Exception as e:
+        return f"Error processing the file: {str(e)}"
